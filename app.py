04c775cfda2c87f7ced9d15b96444d560a48b3b1
Nagasuruthi-R <nagasuruthi6@gmail.com>
2024-07-21 19:39:07 +0530

    filtered bus routes

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import urllib.parse
 
 # Database connection details
 host = "localhost"
-user = "test"
+user = "root"
 password = "Nagunaveen@143"
 database = "bus_informations"
 password_encoded = urllib.parse.quote_plus(password)
@@ -28,10 +28,10 @@ def fetch_data_as_list(query):
     return [row[0] for row in res]
 
 # Streamlit app layout
-st.title('RedBus Data Filtering')
+st.title('RedBus Data')
 
 # Fetch distinct bus routes
-bus_route_query = "SELECT DISTINCT bus_route_name FROM redbus_new_table"
+bus_route_query = "SELECT DISTINCT bus_route_name FROM newbus_route"
 try:
     bus_route_list = fetch_data_as_list(bus_route_query)
 except Exception as e:
@@ -39,7 +39,7 @@ except Exception as e:
     bus_route_list = []
 
 # Fetch distinct bus types
-bus_type_query = "SELECT DISTINCT bus_types_ss FROM redbus_new_table"
+bus_type_query = "SELECT DISTINCT bus_types_ss FROM newbus_route"
 try:
     bus_type_list = fetch_data_as_list(bus_type_query)
 except Exception as e:
@@ -47,12 +47,28 @@ except Exception as e:
     bus_type_list = []
 
 # Fetch distinct seat availabilities
-bus_seat_availability_query = "SELECT DISTINCT seat_availability FROM redbus_new_table"
+bus_seat_availability_query = "SELECT DISTINCT seat_availability FROM newbus_route"
 try:
     bus_seat_list = fetch_data_as_list(bus_seat_availability_query)
 except Exception as e:
     st.error(f"Error fetching seat availabilities: {e}")
     bus_seat_list = []
+
+# # Fetch distinct reaching times
+# reaching_time_query = "SELECT DISTINCT t_reaching_time FROM newbus_route"
+# try:
+#     reaching_time_list = fetch_data_as_list(reaching_time_query)
+# except Exception as e:
+#     st.error(f"Error fetching reaching times: {e}")
+#     reaching_time_list = []
+
+# # Fetch distinct departing times
+# departing_time_query = "SELECT DISTINCT t_departing_time FROM newbus_route"
+# try:
+#     departing_time_list = fetch_data_as_list(departing_time_query)
+# except Exception as e:
+#     st.error(f"Error fetching departing times: {e}")
+#     departing_time_list = []
 
 # Filters
 col1, col2, col3 = st.columns(3)
@@ -67,9 +83,13 @@ col4, col5 = st.columns(2)
 with col4:
     contains_ac = st.radio('AC Bus', options=['Yes', 'No'])
 with col5:
-    star_rating = st.slider('Select Star Rating', 0.0, 5.0, (3.0, 5.0))
+    new_star_ratings = st.slider('Select Star Rating', 0.0, 5.0, (3.0, 5.0))
 
-#seat_availability = st.selectbox('Select Availability', options=bus_seat_list)
+# col6, col7 = st.columns(2)
+# with col6:
+#     reaching_time_filter = st.selectbox('Select Reaching Time', options=reaching_time_list)
+# with col7:
+#     departing_time_filter = st.selectbox('Select Departing Time', options=departing_time_list)
 
 # SQL query with filters
 query = f"""
@@ -79,8 +99,9 @@ FROM redbus_new_table
 WHERE bus_route_name = '{route}'
 AND bus_types_ss = '{bustype}'
 AND contains_ac = '{contains_ac}'
-AND star_rating BETWEEN {star_rating[0]} AND {star_rating[1]}
+AND star_rating BETWEEN {new_star_ratings[0]} AND {new_star_ratings[1]}
 AND price BETWEEN {price_range[0]} AND {price_range[1]}
+
 """
 
 # Debug the query
@@ -91,7 +112,6 @@ try:
     data = fetch_data(query)
     # Convert price and reaching_time_minutes columns to integer
     if not data.empty:
-        #data['price'] = data['price'].astype(int)
         data['reaching_time_minutes'] = data['reaching_time_minutes'].astype(int)
         st.write("### Filtered Results")
         st.dataframe(data)
@@ -100,10 +120,10 @@ try:
 except Exception as e:
     st.error(f"Error fetching data: {e}")
 
-# Display filtered columns on the side
+ # Display filtered columns on the side
 st.sidebar.write("Filtered Columns:")
 if not data.empty:
     st.sidebar.write(data[['bus_route_name', 
                            'reaching_time', 
-                           'price', 
+                            'price', 
                            'seat_availability']])
